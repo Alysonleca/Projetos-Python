847c6a00cbb5e906e095bc841b9e88a850f2f3e0
Alyson Leça <alyson.leca@outlook.com>
2025-02-04 21:29:48 -0300

    continuando

--- Estudos.py
+++ Estudos.py
@@ -1,3 +1,16 @@
 
 test = "restaurante"
-print(test + "\n" + test[2:3])
+print(test + "\n" + test[1:3])
+
+# another
+
+x = "letter to my love"
+x.split()
+
+print(x.split("t"))
+
+result = 80/89
+
+print(f"Result is {result}")
+
+
